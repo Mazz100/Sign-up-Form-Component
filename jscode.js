1f897e6e772720b756698687ee490044b8e2f495
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-01-15 16:24:01 +0200

    Form and Layout update
    - Wrapped subscription and form in another parent for easier grid setup
    
    - Displayed success text after validating the form

--- jscode.js
+++ jscode.js
@@ -20,14 +20,17 @@ function EmailValidate() {
         emailAddress.style.borderColor = 'hsl(0, 100%, 74%)';
         emailError.innerHTML = 'Looks like this is not an email';
         emailAddress.classList.add('error-icon');
+        return false
     }
+
+    return true;
 }
 
 sign_form.addEventListener("submit", submission => {
     submission.preventDefault();
 
     //Validate Test
-    EmailValidate();
+    const isEmailValid = EmailValidate();
 
     if (firstName.value.trim() == '') {
         firstName.style.borderColor = 'hsl(0, 100%, 74%)';
@@ -47,7 +50,35 @@ sign_form.addEventListener("submit", submission => {
         password.classList.add('error-icon');
     }
 
+    if (isEmailValid && firstName.value.trim() !== '' && lastName.value.trim() !== ''
+        && password.value.trim() !== '') {
+        SuccessMessage();
+    }
+
 })
+
+function SuccessMessage() {
+    // Reset all errors and display success text
+    firstnameError.innerHTML = '';
+    firstName.classList.remove('error-icon');
+    firstName.style.borderColor = 'hsla(0, 0%, 75%, 0.7)';
+
+    lastnameError.innerHTML = '';
+    lastName.classList.remove('error-icon');
+    lastName.style.borderColor = 'hsla(0, 0%, 75%, 0.7)';
+
+    passwordError.innerHTML = '';
+    password.classList.remove('error-icon');
+    password.style.borderColor = 'hsla(0, 0%, 75%, 0.7)';
+
+    emailError.innerHTML = '';
+    emailAddress.style.borderColor = 'hsla(0, 0%, 75%, 0.7)';
+    emailAddress.classList.remove('error-icon');
+
+    //Display the Success Text
+    claimedText.classList.add('claimed-trial');
+    claimedText.innerHTML = 'Thanks for claiming trial!';
+}
 
 
 sign_form.addEventListener("input", input => {
@@ -55,7 +86,7 @@ sign_form.addEventListener("input", input => {
     if (firstName.value.trim() !== '') {
         firstnameError.innerHTML = '';
         firstName.classList.remove('error-icon');
-        firstName.style.borderColor = 'hsla(0, 0%, 75%, 0.7)';;
+        firstName.style.borderColor = 'hsla(0, 0%, 75%, 0.7)';
     }
 
     if (lastName.value.trim() !== '') {
